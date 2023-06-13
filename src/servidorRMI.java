cbee9fb955f4f736000508b46170f3ffd1cf87c2
ocurrentduke1 <a21110128@ceti.mx>
2023-06-13 14:50:58 -0600

    Initial commit

---
+++ src/servidorRMI.java
@@ -0,0 +1,17 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public class servidorRMI {
+    public static void main(String[] args) throws RemoteException{
+        int port = 1005;
+        try{
+            System.setProperty("java.rmi.server.hostname","192.168.137.115");
+            Registry rmi = LocateRegistry.createRegistry(port);
+            rmi.rebind("Chat", (Remote) new implementacionChat());
+            System.out.println("Servidor Activo");
+        } catch(RemoteException e){}
+    }
+}
+
