d1a93c8eb873bb10b052deabc1c5a65eb2881258
ocurrentduke1 <a21110128@ceti.mx>
2023-06-13 17:34:55 -0600

    update

--- src/servidorRMI.java
+++ src/servidorRMI.java
@@ -1,19 +1,22 @@
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 
 public class servidorRMI {
-    // public static String HOST = "192.168.100.28";
-    public static String HOST = "192.168.100.8";
+    public static String HOST = "192.168.100.28";
     public static int PORT = 1099;
-    public static void main(String[] args) throws RemoteException{
-        try{
+
+    public servidorRMI() {
+    }
+
+    public static void main(String[] var0) throws RemoteException {
+        try {
             System.setProperty("java.rmi.server.hostname", HOST);
-            Registry rmi = LocateRegistry.createRegistry(PORT);
-            rmi.rebind("Chat", (Remote) new implementacionChat());
+            Registry var1 = LocateRegistry.createRegistry(PORT);
+            var1.rebind("Chat", new implementacionChat());
             System.out.println("Servidor Activo");
-        } catch(RemoteException e){}
+        } catch (RemoteException var2) {
+        }
+
     }
 }
-
